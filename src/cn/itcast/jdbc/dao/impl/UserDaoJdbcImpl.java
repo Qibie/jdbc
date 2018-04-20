4c9a666f7fe9e84a9afeba1082af71c19050451a
Qibie <472297371@qq.com>
2018-04-21 00:46:25 +0800

    2018-04-24

---
+++ src/cn/itcast/jdbc/dao/impl/UserDaoJdbcImpl.java
@@ -0,0 +1,38 @@
+package cn.itcast.jdbc.dao.impl;
+
+import cn.itcast.jdbc.dao.UserDao;
+import cn.itcast.jdbc.domain.User;
+
+public class UserDaoJdbcImpl implements UserDao {
+
+	@Override
+	public void addUser(User User) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User getUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findUser(String loginName, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
