e8d917108acacd2f59248d2ea188ac1116e75595
Qibie <472297371@qq.com>
2018-04-21 00:46:00 +0800

    2018-04-24

---
+++ src/cn/itcast/jdbc/dao/UserDao.java
@@ -0,0 +1,20 @@
+package cn.itcast.jdbc.dao;
+
+import cn.itcast.jdbc.domain.User;
+
+/**
+ * 
+ * @author HASEE
+ *
+ */
+public interface UserDao {
+	public abstract void addUser(User User);
+
+	User getUser(int userId);
+
+	User findUser(String loginName, String password);
+
+	void update(User user);
+
+	void delete(User user);
+}
